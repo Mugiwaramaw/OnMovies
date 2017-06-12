500d7c9371b3e72f284e686f34e119d4ce4f335c
Mugiwaramaw <rizkimawan83@gmail.com>
2017-06-12 13:11:11 +0700

    Initial commit

---
+++ app/src/main/java/com/mattbozelka/popularmovies/Config/Config.java
@@ -0,0 +1,9 @@
+package com.mattbozelka.popularmovies.Config;
+
+/**
+ * Configuration file for app needed info. Currently just the API Key
+ */
+
+public class Config {
+    public static final String API_KEY = "df09858eb8a643e3b8f84f0b717b97ce";
+}
